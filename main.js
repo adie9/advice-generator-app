c7def0b841ab1328f3a6ee257bb63fb4f0b1598a
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2023-03-30 13:04:28 -0400

    add api data to page

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
-// Fetch data from API
+// Create variables
 let id;
 let quote;
 
-let number = document.querySelector("#id_num");
+const number = document.querySelector("h1");
+const advice = document.querySelector("blockquote");
 
+// Fetch data from API
 fetch("https://api.adviceslip.com/advice")
     .then(response => response.json())
     
-    .then(data => console.log(data.slip))
+    .then(data => {
+        console.log(data.slip);
+        id = data.slip.id;
+        quote = data.slip.advice;
+        console.log(id);
+        console.log(quote);
+
+        number.innerText = `Advice #${id}`;
+        advice.innerText = `${quote}`;
         
-    
-    
-    
-    
+    })
+        
     .catch(error => console.log("Error"))
+
+
 
 
