38a1d966f5aa80fb582d5e2dfccb65eaa186c3db
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2023-03-30 15:19:16 -0400

    added active state

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ let quote;
 
 const number = document.querySelector("h1");
 const advice = document.querySelector("blockquote");
+const get_advice = document.querySelector(".dice-container");
 
-// Fetch data from API
+// Fetch data from API initially
 fetch("https://api.adviceslip.com/advice")
     .then(response => response.json())
     
@@ -19,11 +20,33 @@ fetch("https://api.adviceslip.com/advice")
         console.log(quote);
 
         number.innerText = `Advice #${id}`;
-        advice.innerText = `"${quote}"`;
+        advice.innerText = `" ${quote} "`;
         
     })
         
     .catch(error => console.log("Error"))
+
+// Fetch data on button click
+get_advice.addEventListener("click", function() {
+    fetch("https://api.adviceslip.com/advice")
+    .then(response => response.json())
+    
+    .then(data => {
+        console.log(data.slip);
+
+        id = data.slip.id;
+        quote = data.slip.advice;
+
+        console.log(id);
+        console.log(quote);
+
+        number.innerText = `Advice #${id}`;
+        advice.innerText = `" ${quote} "`;
+        
+    })
+        
+    .catch(error => console.log("Error"))
+})
 
 
 
